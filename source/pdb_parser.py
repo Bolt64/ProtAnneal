cbd05e99749bd6f99c2798eae1d7abb083311f89
Sayantan Khan <bolt.khan@gmail.com>
2015-03-11 21:16:38 +0530

    Finished up the residue_mutator.py

--- source/pdb_parser.py
+++ source/pdb_parser.py
@@ -3,20 +3,49 @@
 def parse_file(pdb_file):
     for line in open(pdb_file):
         temp=line.strip().ljust(80," ")
-        line_output={}
-        line_output['ATOM']=temp[0:6]
-        line_output['serial']=int(temp[6:11])
-        line_output['name']=temp[12:16]
-        line_output['altLoc']=temp[16]
-        line_output['resName']=temp[17:20]
-        line_output['chainID']=temp[21]
-        line_output['resSeq']=int(temp[22:26])
-        line_output['iCode']=temp[26]
-        line_output['x']=float(temp[30:38])
-        line_output['y']=float(temp[38:46])
-        line_output['z']=float(temp[46:54])
-        line_output['occupancy']=float(temp[54:60])
-        line_output['tempFactor']=float(temp[60:66])
-        line_output['element']=temp[76:78]
-        line_output['charge']=temp[78:80]
-        yield line_output
+        if temp[0:6]=="ATOM  ":
+            line_output={}
+            line_output['ATOM']=temp[0:6]
+            line_output['serial']=int(temp[6:11])
+            line_output['name']=temp[12:16]
+            line_output['altLoc']=temp[16]
+            line_output['resName']=temp[17:20]
+            line_output['chainID']=temp[21]
+            line_output['resSeq']=int(temp[22:26])
+            line_output['iCode']=temp[26]
+            line_output['x']=float(temp[30:38])
+            line_output['y']=float(temp[38:46])
+            line_output['z']=float(temp[46:54])
+            line_output['occupancy']=float(temp[54:60])
+            line_output['tempFactor']=float(temp[60:66])
+            line_output['element']=temp[76:78]
+            line_output['charge']=temp[78:80]
+            yield line_output
+
+def pdb_format(line):
+    out_string=""
+    out_string+=line['ATOM']
+    out_string+=str(line['serial']).rjust(5)
+    out_string+=" "
+    out_string+=line['name']
+    out_string+=line['altLoc']
+    out_string+=line['resName']
+    out_string+=" "
+    out_string+=line['chainID']
+    out_string+=str(line['resSeq']).rjust(4)
+    out_string+=line['iCode']
+    out_string+=" "*3
+    out_string+=("{0:.3f}".format(line['x'])).rjust(8)
+    out_string+=("{0:.3f}".format(line['y'])).rjust(8)
+    out_string+=("{0:.3f}".format(line['z'])).rjust(8)
+    out_string+=("{0:.2f}".format(line['occupancy'])).rjust(6)
+    out_string+=("{0:.2f}".format(line['tempFactor'])).rjust(6)
+    out_string+=" "*10
+    out_string+=line['element']
+    out_string+=line['charge']
+    return out_string
+
+if __name__=="__main__":
+    from sys import argv
+    for l in parse_file(argv[-1]):
+        print(pdb_format(l))
--- source/residue_mutator.py
+++ source/residue_mutator.py
@@ -4,5 +4,61 @@ import numpy as np
 import kabsch as kb
 import pdb_parser as parser
 
-def rotamer(protein_pdb, res_no, res_pdb):
-    pass
+# This script may not be 100% correct. It has not been tested
+# rigourously enough. Unless tested further, do not base any scientific
+# data on the results of this script. You have been warned.
+
+def get_backbone(pdb_file):
+    """
+    Gets the N, CA, C backbone of the pdb file
+    """
+    backbone=[]
+    for line in parser.parse_file(pdb_file):
+        if line['ATOM']=="ATOM  ":
+            if line['name'] in (' N  ', ' CA ', ' C  '):
+                backbone.append([line['x'],line['y'],line['z']])
+    return np.matrix(backbone)
+
+def mutate(protein_pdb, res_no, res_pdb):
+    """
+    It replaces the residue on protein at res_no
+    position with the new residue, res_pdb.
+    This function may be dodgy. DO NOT USE without further testing.
+    You have been warned.
+    """
+    backbone_original=[]
+    for line in parser.parse_file(protein_pdb):
+        if line['ATOM']=="ATOM  " and line['resSeq']==res_no:
+            if line['name'] in (' N  ', ' CA ', ' C  '):
+                backbone_original.append([line['x'], line['y'], line['z']])
+    backbone_replacement=get_backbone(res_pdb).tolist()
+# Should the arguments be switched around?
+    rotation=kb.kabsch(np.matrix(backbone_original), np.matrix(backbone_replacement))
+    centroid_original=kb.get_centroid(np.matrix(backbone_original))
+    centroid_replacement=kb.get_centroid(np.matrix(backbone_replacement))
+    inverted_centroid=[-1*i for i in centroid_replacement]
+    new_protein=[]
+    amino_acid_inserted=False
+    for l in parser.parse_file(protein_pdb):
+        if l['resSeq']!=res_no:
+            new_protein.append(l)
+        if l['resSeq']==res_no and not amino_acid_inserted:
+            for j in parser.parse_file(res_pdb):
+                location=np.matrix([[j['x'],j['y'], j['z']]])
+                new_loc=kb.translate((kb.translate(location, inverted_centroid))*rotation, centroid_original)
+                x,y,z=new_loc.tolist()[0]
+                j['x']=x
+                j['y']=y
+                j['z']=z
+                j['resSeq']=res_no
+                new_protein.append(j)
+            amino_acid_inserted=True
+    return new_protein
+
+if __name__=="__main__":
+    from sys import argv
+    protein_pdb=argv[-3]
+    res_no=int(argv[-2])
+    res_pdb=argv[-1]
+    for l in mutate(protein_pdb, res_no, res_pdb):
+        print(parser.pdb_format(l))
