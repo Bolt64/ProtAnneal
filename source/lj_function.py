ccd4423ee704de04f1dcd41d8df253f988e1ec68
Sayantan Khan <bolt.khan@gmail.com>
2015-03-18 21:56:33 +0530

    Late night commit

--- source/lj_function.py
+++ source/lj_function.py
@@ -1,31 +1,37 @@
 #usr/bin/python
 
 import math
-import sys
+from sys import argv
 import matplotlib.pyplot as plt
 
-def lj_potential(dist, rm):
-    lj = 0
-    for i in range(0, len(dist)):
-        lj += math.pow((rm/dist[i]), 12) - 2*math.pow((rm/dist[i]), 6)
-    return lj
+def lj_potential(distance, rm):
+    return math.pow((rm/distance), 12) - 2*math.pow((rm/distance), 6)
 
 def extract(filename):
-    distances = []
     for line in open(filename):
-        distances.append(float(line.strip()))
-    return distances
+        yield float(line.strip())
 
-if __name__ == '__main__':
-    pot = []
-    rm = []
-    d = extract(sys.argv[1])
-    for i in range (0, 100):
-        rm.append(i*0.1)
-        v = lj_potential(d, i*0.5)
-        pot.append(v)
-    plt.title('Lennard-Johnes Plot of '+str(sys.argv[1]))
-    plt.xlabel('Rm ---->')
-    plt.ylabel('LJ-Potential ---->')
-    plt.plot(pot, rm, 'ro')
-    plt.savefig('LJ-potential-'+str(sys.argv[1])+'.jpeg')
+def get_total_potential(distance_file, rm, potential_function):
+    return sum(potential_function(distance, rm) for distance in extract(distance_file))
+
+def vary_across_rm(distance_file, rm_range, step_size, potential_function):
+    lower,upper=rm_range
+    current_rm=lower
+    while current_rm < upper:
+        yield current_rm, get_total_potential(distance_file, current_rm, potential_function)
+        current_rm += step_size
+
+def get_min_rm(distance_file, rm_range, step_size, potential_function):
+    return min((i[1],i[0]) for i in vary_across_rm(distance_file, rm_range, step_size, potential_function))[1]
+
+
+if __name__=="__main__":
+    distance_file=argv[-3]
+    lower=float(argv[-2])
+    upper=float(argv[-1])
+    step_size=0.1
+    xy=[i for i in vary_across_rm(distance_file, (lower, upper), step_size, lj_potential)]
+    x=[i[0] for i in xy]
+    y=[i[1] for i in xy]
+    plt.plot(x,y,'ro')
+    plt.savefig("output")
