0e19b30dbf2855dcb45aa851561acbd3539433ba
Sayantan Khan <bolt.khan@gmail.com>
2015-04-05 15:43:31 +0530

    Added some more stuff.

--- source/config.py
+++ source/config.py
@@ -4,11 +4,11 @@ of the program.
 """
 
 # Location constants
-residue_path = "/home/bolt/protein_lab/database/residues/{0}_naiveH.pdb"
-naccess_location = "/home/bolt/protein_lab/third-party/binaries/naccess2.1.1/naccess"
-reduce_location = "/home/bolt/protein_lab/third-party/binaries/reduce"
+residue_path = "/home/bolt/protanneal/database/residues/{0}_naiveH.pdb"
+naccess_location = "/home/bolt/protanneal/third-party/binaries/naccess2.1.1/naccess"
+reduce_location = "/home/bolt/protanneal/third-party/binaries/reduce"
 Zdope_command = "/usr/local/bin/Zdope"
-dunbrack_file = "/home/bolt/protein_lab/other/dunbrack2.csv"
+dunbrack_file = "/home/bolt/protanneal/other/dunbrack2.csv"
 output_directory = "../workspace/temp_pdb"
 result_directory = "../workspace/result_pdb"
 
